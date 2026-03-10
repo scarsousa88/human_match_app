4ba64bc406d7f7ff5723a08554a6afa9e23e6faf
o.pinto <oscardiogopinto@gmail.com>
2026-03-10 02:48:18 +0000

    Pre 1.1.0 - Inclusão da Astrologia - Inclusão do human graph no local correto - alteração do ordenamento da secçao do human design - Explicação da info do human design - Mais info de Astrologia - COrreção da fórmula de calculo -Reposição do loader personalizado

--- lib/ui/loading_widget.dart
+++ lib/ui/loading_widget.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class LoadingWidget extends StatelessWidget {
+class LoadingWidget extends StatefulWidget {
   final double size;
-  const LoadingWidget({super.key, this.size = 40.0});
+  const LoadingWidget({super.key, this.size = 60.0});
+
+  @override
+  State<LoadingWidget> createState() => _LoadingWidgetState();
+}
+
+class _LoadingWidgetState extends State<LoadingWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SizedBox(
-        width: size,
-        height: size,
-        child: const CircularProgressIndicator(
-          strokeWidth: 3,
-          valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFE6B325)),
+      child: RotationTransition(
+        turns: _controller,
+        child: SvgPicture.asset(
+          'assets/icon/loading.svg',
+          width: widget.size,
+          height: widget.size,
         ),
       ),
     );
